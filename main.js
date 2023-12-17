2606fa1ea34cae01856ed4b771433b95e438aafb
Jeremy Dunn <jeremydunn@Jeremys-MacBook-Pro.local>
2023-12-17 17:55:01 -0500

    Add secondary smaller header bar
    Main header bar looked terrible on smaller screens, so added the ability
    to choose a smaller header bar if the screen is small enough

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-import { addLargeHeaderBar } from './scripts/header-bar.js';
+import { addLargeHeaderBar, addSmallHeaderBar } from './scripts/header-bar.js';
 
 const headerBar = document.getElementById('header-bar');
-addLargeHeaderBar(headerBar);
+
+window.innerWidth < 1100 ? addSmallHeaderBar(headerBar) : addLargeHeaderBar(headerBar);
+window.addEventListener('resize', () => {
+    document.getElementById('header-dynamic').remove();
+    window.innerWidth < 1100 ? addSmallHeaderBar(headerBar) : addLargeHeaderBar(headerBar);
+});
 
 if (headerBar.classList.contains('blog-header')) {
     document.getElementById('blog-button').classList.add('active');
